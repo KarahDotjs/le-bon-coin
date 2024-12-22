97cdd5ef941466e0f4ef74537d2f789ee209cfd7
«KarahDotjs » <bintoukaramoko3@gmail.com>
2024-12-22 14:46:43 +0100

    Added offer pictures

---
+++ src/components/OfferCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  offerInfos: Object,
+})
+
+console.log(props)
+</script>
+
+<template>
+  <div>
+    <img :src="offerInfos.attributes.pictures.data[0].attributes.url" alt="" />
+
+    <p>{{ offerInfos.attributes.title }}</p>
+    <p>{{ offerInfos.attributes.price }}€</p>
+    <p>{{ offerInfos.attributes.publishedAt }}</p>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,41 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+axios
+
+import OfferCard from '@/components/OfferCard.vue'
+
+const offerslist = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      'https://site--strapileboncoin--2m8zk47gvydr.code.run/api/offers?populate[0]=pictures&populate[1]=owner.avatar',
+    )
+    console.log(data.data)
+
+    offerslist.value = data.data
+  } catch (error) {
+    console.log(error)
+  }
+})
+</script>
 
 <template>
   <main>
-    <!-- <h1>Hello World !</h1> -->
+    <div class="container">
+      <p>Des millions de petites annonces et autant d’occasions de se faire plaisir</p>
+      <div>
+        <p>C'est le moment de vendre</p>
+        <button>
+          <font-awesome-icon :icon="['far', 'plus-square']" />
+          Déposer une annonce
+        </button>
+      </div>
+
+      <div>
+        <OfferCard v-for="offer in offerslist" :key="offer.id" :offerInfos="offer" />
+      </div>
+    </div>
   </main>
 </template>
